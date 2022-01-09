202db6488d10980ddd8e1533e59ef7dfca98ae21
nianny <xiaonianhe@hotmail.com>
2022-01-09 09:14:25 +0800

    oops

---
+++ src/main/java/nianny/hallo/coins/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package nianny.hallo.coins.mixin;
+
+import nianny.hallo.coins.HalloCoins;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		HalloCoins.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
