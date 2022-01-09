826cf636f4cfdd691633de32d0e55f3bf64322ef
AV3_08 <66512356+AV306@users.noreply.github.com>
2022-01-09 10:20:27 +0800

    Attempt an itemgroup
    I’m bad at thisc

--- src/main/java/nianny/hallo/coins/HalloCoins.java
+++ src/main/java/nianny/hallo/coins/HalloCoins.java
@@ -14,16 +14,28 @@ public class HalloCoins implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LogManager.getLogger("hallocoins");
+	
+	// Custom item group (seems appropriate)
+	public static final ItemGroup HALLOCOIN_GROUP = FabricItemGroup.create(
+		new Identifier("hallocoins", "general")) // translation key for group name (full translation key: itemGroup.hallocoins.general)
+		.icon(() -> new ItemStack(BASIC_COIN)) // FIXME: may break
+		.appendItens(stacks -> { stacks.add(BASIC_COIN); }) // FIXME: ^ see above comment
+		.build();
+	
+	// Create item objects
+	public static final Item BASIC_COIN = new Item(new Item.Settings().group(HALLOCOIN_GROUP);
+	
+	
 
-	public static final Item BASIC_COIN = new Item(new Item.Settings().group(ItemGroup.MISC));
-
+	
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		LOGGER.info("HalloCoin is initialising!");
+		
+		
+		// Register HalloCoin
 		Registry.register(Registry.ITEM, new Identifier("hallocoins", "basic_coin"), BASIC_COIN);
 
-		LOGGER.info("Hello Fabric world!");
+
 	}
 }
