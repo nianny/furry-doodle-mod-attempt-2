a7c1b0122af1854ae6e468cefb909b0738e370d8
AV3_08 <66512356+AV306@users.noreply.github.com>
2022-01-09 10:22:54 +0800

    oops

--- src/main/java/nianny/hallo/coins/HalloCoins.java
+++ src/main/java/nianny/hallo/coins/HalloCoins.java
@@ -1,8 +1,10 @@
 package nianny.hallo.coins;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
