6da2f5dffdbd1240afd74e41b4e9abe998702860
AV3_08 <66512356+AV306@users.noreply.github.com>
2022-01-17 11:38:46 +0800

    god my eyes

--- src/main/java/nianny/hallo/coins/HalloCoinItem.java
+++ src/main/java/nianny/hallo/coins/HalloCoinItem.java
@@ -6,54 +6,76 @@ import net.minecraft.util.Pair;
 
 import java.util.*;
 
+/*
+@YeetBot3276:
+This is Java, not C#, so could we please stick to default Java formatting?
+e.g. lowercase method names pls
+also PLEASE leave spaces where appropriate and not compress everything, it makes it WAY more readable.
+(NOTE: You don't have do do it if you really don't want to)
+Thanks!
+- @AV306
+*/
+
 public class HalloCoinItem extends Item {
     private static final Comparator<Pair<String, Integer>> byValue = Comparator.comparingInt(Pair::getRight);
+    
     public static Map<String, Integer> val = new HashMap<>();
+    
     public HalloCoinItem(Settings settings,String name, int value) {
         super(settings);
-        val.put(name, value); //store type of coin to value
+        val.put(name, value); // store type of coin to value
         HalloCoins.convert.put(name,this);
     }
-    public static int GetBalance(PlayerEntity player){
-        UUID playeruuid = player.getUuid(); //get UUID cuz usernames might be the same with cracked launchers
+    
+    public static int GetBalance(PlayerEntity player) {
+        UUID playeruuid = player.getUuid(); // get UUID cuz usernames might be the same with cracked launchers
         return HalloCoins.mp.get(playeruuid);
     }
+    
     public static Map<String, Integer> Withdraw(PlayerEntity player, int withAmount){
-         Map<String,Integer> welp = new HashMap<>();
+        Map<String,Integer> welp = new HashMap<>();
+       
         int bal = GetBalance(player);
+        
         try {
             HalloCoins.mp.replace(player.getUuid(), bal, bal - withAmount); //update balance
-        }catch(Exception e){
+        } catch (Exception e) {
             throw new AssertionError("That was NOT supposed to happen...");
         }
+        
         List<Pair<String,Integer>> st = new ArrayList<>();
 
-        for(var entry: val.entrySet()){
+        for(var entry: val.entrySet()) {
             st.add(new Pair<>(entry.getKey(),entry.getValue()));
         }
+        
         st.sort(byValue);
+        
         Collections.reverse(st);
-        for(var entry:st){
+        
+        for(var entry:st) {
             int t = withAmount / entry.getRight();
             welp.put(entry.getLeft(), t);
             //HalloCoins.LOGGER.info("t = " + t + ", \nentry.getLeft() = " + entry.getLeft() + ",\n entry.getRight() = "+ entry.getRight() + ", \n withdrawalamt = " + withAmount);
             withAmount -= t*entry.getRight();
         }
+        
         return welp;
     }
+    
     public static int GetBalanceFromCoin(String coin, int count){ // can only dep one type of coin at a time cuz mainhand/offhand can only hold 1 type of item
-        try{
+        try {
             return val.get(coin) * count;
-        }catch(Exception e){
-            throw new AssertionError("Coin not defined!" ); //should not run by right
+        } catch(Exception e){
+            throw new AssertionError("Coin not defined!" ); // should not run by right
         }
     }
 
-    public static void Deposit(String coin, int count, PlayerEntity player){
+    public static void Deposit(String coin, int count, PlayerEntity player) {
         int bal = GetBalance(player);
         try {
             HalloCoins.mp.replace(player.getUuid(), bal, bal + GetBalanceFromCoin(coin, count)); //update balance
-        }catch(Exception e){
+        } catch(Exception e) {
             throw new AssertionError("That was NOT supposed to happen...");
         }
     }
